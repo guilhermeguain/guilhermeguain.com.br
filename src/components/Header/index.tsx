ab780a765bff952726358163b8ab7b37b17d5aea
Guilherme Guain <guilherme.guain@gmail.com>
2020-11-01 20:55:01 -0300

    added mobile menu + header adjusts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import HamburgerMenu from 'react-hamburger-menu';
 
 import './styles.css';
 
-import Menu from '../Menu';
+import menuItems from '../../menu.json';
 
 import logoImg from '../../images/logo.svg';
 
+interface WindowSizeData {
+  width: undefined|number;
+  height: undefined|number;
+}
+
 function Header() {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [windowSize, setWindowSize] = useState<WindowSizeData>({
+    width: undefined,
+    height: undefined
+  });
+
+  const isMobile = windowSize.width && windowSize.width < 768;
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight
+      });
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+
   function handleLogoClick(event: React.MouseEvent<HTMLElement>) {
     event.preventDefault();
 
     window.scrollTo(0, 0);
+  }
+
+  function handleHamburgerClick() {
+    setToggleMenu(toggleMenu ? false : true);
+  }
+
+  function handleMenuClick(event: React.MouseEvent<HTMLElement>, target: string) {
+    event.preventDefault();
+
+    const header = document.getElementById('header');
+    const element = document.getElementById(target);
+
+    const headerHeight = header?.getBoundingClientRect().height || 0;
+    const elementOffsetTop = element?.offsetTop || 0;
+
+    setToggleMenu(false);
+
+    window.scrollTo(0, elementOffsetTop - headerHeight);
   }
 
   return (
@@ -19,7 +67,29 @@ function Header() {
         <a className="header-logo" href="#hero" onClick={ event => handleLogoClick(event) }>
           <img className="header-logo" src={logoImg} width="495" height="40" alt="Guilherme Guain" title="Guilherme Guain" />
         </a>
-        <Menu />
+        <div className={`mobile-menu-overlay ${toggleMenu ? "active" : "" }`} onClick={ () => { setToggleMenu(false) } }></div>
+        { (toggleMenu || !isMobile) && (
+          <ul className={` nav-menu ${isMobile ? "mobile-menu" : "main-menu"} `}>
+            {menuItems.map(menuItem => (
+              <li key={menuItem.title}>
+                <a href={`#${menuItem.target}`} onClick={ event => { handleMenuClick(event, menuItem.target) } }>{menuItem.label}</a>
+              </li>
+            ))}
+          </ul>
+        ) }
+        { isMobile &&
+          <HamburgerMenu
+            isOpen={toggleMenu}
+            menuClicked={handleHamburgerClick}
+            width={18}
+            height={16}
+            strokeWidth={2}
+            rotate={0}
+            color='#282933'
+            borderRadius={0}
+            animationDuration={0.5}
+          />
+        }       
       </div>
     </div>
   );
